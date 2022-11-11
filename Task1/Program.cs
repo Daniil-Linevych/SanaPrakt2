853d51f65e509fd1d256a9870e68c6d66a78db2c
daniil_linevych21 <dan4224rit@gmail.com>
2022-11-11 17:12:16 +0200

    change 1,2,3 messages

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp(change in 1 string)");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp(change in 2 string)");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp(change in 3 string)");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
